fc30379f2e958616fb838458bfd8966a494959e8
ar-erik.blanca <erik.bg7@gmail.com>
2021-04-06 20:12:23 +0200

    Display search confidence in results screen

--- lib/screens/search/components/search_results.dart
+++ lib/screens/search/components/search_results.dart
@@ -1,21 +1,23 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:futter_project_tfg/screens/search/components/search_confidence.dart';
 import 'package:futter_project_tfg/screens/search/components/search_ttle.dart';
 
 class SearchResults extends StatelessWidget {
   final File image;
   final String result;
+  final double confidence;
 
-  const SearchResults({Key key, this.image, this.result}) : super(key: key);
+  const SearchResults({Key key, this.image, this.result, this.confidence})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        SearchTitle(
-          text: 'RESULTATS',
-        ),
+        SearchTitle(text: 'RESULTATS'),
+        SearchConfidence(confidence: confidence),
         Container(
           width: 150.0,
           height: 150.0,
