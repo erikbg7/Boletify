91a2cc832343544f1c351cfd9544895bb81400d2
ar-erik.blanca <erik.bg7@gmail.com>
2021-05-23 00:33:52 +0200

    Detail Screen handle no network connection UI

--- lib/screens/detail/detail_screen.dart
+++ lib/screens/detail/detail_screen.dart
@@ -2,17 +2,19 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:futter_project_tfg/models/mushroom_info_model.dart';
+import 'package:futter_project_tfg/widgets/custom_icon.dart';
 
 class DetailScreen extends StatelessWidget {
-
   final File image;
   final MushroomInfo mushroom;
+  final String notFoundMessage =
+      'No hi ha informació disponible per aquest bolet actualment';
 
   const DetailScreen({Key key, this.image, this.mushroom}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
+//    mushroom.nameAKA.isNotEmpty
     return Scaffold(
       body: Column(
         children: [
@@ -32,27 +34,41 @@ class DetailScreen extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            child: Column(
-              children: mushroom.name.isEmpty
-                  ? [
-                Text(
-                    'No hi ha informació disponible per aquest bolet actualment')
-              ]
-                  : [
-                Text(mushroom.name),
-                Text(mushroom.nameScientific),
-                Text(mushroom.nameAKA),
-                Text(mushroom.description),
-                Text(mushroom.location),
-                Text(mushroom.observations),
-                Text(mushroom.season),
-                Text(mushroom.edibility.toString()),
-              ],
+          Expanded(
+            flex: 1,
+            child: new SingleChildScrollView(
+              scrollDirection: Axis.vertical, //.horizontal
+              child: Column(
+                children: mushroom.name.isEmpty
+                    ? [Text(notFoundMessage)]
+                    : [
+                  CustomIcon(type: EdibleType.poisonous, image: 'assets/edibility/edible.png', tooltip: 'this is a tooltip', color: Colors.green,),
+                  Text(mushroom.name),
+                        Text(mushroom.nameScientific),
+                        if (mushroom.nameAKA.isEmpty) Text('Second Widget'),
+                        Text(mushroom.season),
+                        Text(mushroom.edibility.toString()),
+                        Text(mushroom.description),
+                        Text(mushroom.description),
+                        Text(mushroom.description),
+                        Text(mushroom.location),
+                        Text(mushroom.observations),
+                        Text(mushroom.observations),
+                        Text(mushroom.observations),
+                        Text(mushroom.observations),
+                        Text(mushroom.observations),
+                      ],
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
+  }
+
+  getInformationBlock(String text) {
+    if (text.isNotEmpty) {
+      return Text(text);
+    }
   }
 }
