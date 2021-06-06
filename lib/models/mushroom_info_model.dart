746a5c0f60b866fe946077a6ca707b14188d0ce6
ar-erik.blanca <erik.bg7@gmail.com>
2021-06-06 14:09:07 +0200

    Add mushrooms mocked list

--- lib/models/mushroom_info_model.dart
+++ lib/models/mushroom_info_model.dart
@@ -5,7 +5,7 @@ import 'mushroom_label_model.dart';
 class MushroomInfo {
   final String name;
   final String nameScientific;
-  final List<MushroomLabel> labels;
+  final List<SearchLabels> labels;
   String nameAKA;
   String description;
   String location;
@@ -21,18 +21,24 @@ class MushroomInfo {
       this.observations = '',
       this.edibility = 0});
 
-  toConcatString(){
-    return '$name, $nameScientific';
+  toConcatString() {
+    var labelsString = '';
+    labels.forEach((element) {
+      labelsString = '$labelsString $element -';
+    });
+    return '$name, $nameScientific, $labelsString';
   }
 
   factory MushroomInfo.fromFirestore(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data();
-    return MushroomInfo(data['name'] ?? 'unknown', data['scientificName'] ?? '', []);
+    return MushroomInfo(
+        data['name'] ?? 'unknown', data['scientificName'] ?? '', []);
   }
 }
 
 getMushrooms() {
-  final MushroomInfo amanita = new MushroomInfo('Amanita', 'Amanita Muscaria', [],
+  final MushroomInfo amanita = new MushroomInfo(
+      'Amanita', 'Amanita Muscaria', [],
       nameAKA: '',
       description:
           'Es una seta tóxica que provoca intoxicaciones que se manifiestan al cabo de poco rato de su ingestión (normalmente entre 1 y 4 horas) con trastornos digestivos (diarrea, vómitos, sequedad de boca), aceleración del pulso y trastornos de tipo nervioso: delirio, excitación, alucinaciones, confusión mental.',
@@ -44,4 +50,16 @@ getMushrooms() {
   final List<MushroomInfo> mushroomDDBB = [];
   mushroomDDBB.add(amanita);
   return mushroomDDBB;
+}
+
+List<MushroomInfo> getMushroomsListMock() {
+  final List<MushroomInfo> mushroomsList = [];
+  mushroomsList.add(MushroomInfo("1", "1", [SearchLabels.autumn]));
+  mushroomsList
+      .add(MushroomInfo("2", "2", [SearchLabels.autumn, SearchLabels.summer]));
+  mushroomsList.add(MushroomInfo("3", "3",
+      [SearchLabels.autumn, SearchLabels.summer, SearchLabels.toxic]));
+
+  return mushroomsList;
+
 }
