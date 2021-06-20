d3b578d1805a15d7212ddb72cf2bd9e1ed8b88f2
ar-erik.blanca <erik.bg7@gmail.com>
2021-06-20 17:51:08 +0200

    Add unit tests :white_check_mark:

---
+++ test/home_card_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:futter_project_tfg/models/mushroom_label_model.dart';
+import 'package:futter_project_tfg/screens/home/components/home_card.dart';
+import 'package:futter_project_tfg/widgets/custom_icon.dart';
+
+Widget buildTestableWidget(Widget widget) {
+  return MediaQuery(data: MediaQueryData(), child: MaterialApp(home: widget));
+}
+
+void main() {
+  testWidgets(
+    'Displays a Card widget with text and CustomIcon',
+    (WidgetTester tester) async {
+      final SearchLabels label = SearchLabels.winter;
+      final MushroomLabel mushroom = mushroomLabels[label];
+      await tester.pumpWidget(buildTestableWidget(HomeCard(label: label)));
+
+      expect(find.text(mushroom.tooltip), findsOneWidget);
+      expect(find.byType(CustomIcon), findsOneWidget);
+      expect(find.byType(LinearGradient), findsOneWidget);
+    },
+  );
+}
