5ee73b1a07762d6d661af7a0ec7406e336c29ae7
ar-erik.blanca <erik.bg7@gmail.com>
2021-06-27 01:20:18 +0200

    Improve coverage ✅

--- test/settings_test.dart
+++ test/settings_test.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:futter_project_tfg/config/settings_items_config.dart';
+import 'package:futter_project_tfg/screens/settings/credits/credits_screen.dart';
+import 'package:futter_project_tfg/screens/settings/privacy/privacy_screen.dart';
 import 'package:futter_project_tfg/screens/settings/settings_screen.dart';
 
 import 'utils/index.dart';
 
 void main() {
+  testWidgets(
+    'Displays a page for Settings/Credits',
+    (WidgetTester tester) async {
+      await tester
+          .pumpWidget(buildTestableWidget(Scaffold(body: CreditsScreen())));
+      expect(find.text('Credits'), findsOneWidget);
+      expect(find.byType(Column), findsOneWidget);
+      expect(
+          find.text('wwww.github.com/erikbg7/Flutter-Project'), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'Displays a page for Settings/Privacy',
+    (WidgetTester tester) async {
+      await tester
+          .pumpWidget(buildTestableWidget(Scaffold(body: PrivacyScreen())));
+      expect(find.text('Privacitat'), findsOneWidget);
+      expect(find.byType(Center), findsOneWidget);
+      expect(find.byType(Text), findsWidgets);
+    },
+  );
+
   testWidgets(
     'Displays a list of settings',
     (WidgetTester tester) async {
