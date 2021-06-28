675aa26d1ce9ae1182080ee957826dcdddf5b8b4
ar-erik.blanca <erik.bg7@gmail.com>
2021-06-28 22:49:52 +0200

    Improve testing structure :white_check_mark:

--- test/settings_test.dart
+++ test/settings_test.dart
@@ -7,39 +7,32 @@ import 'package:futter_project_tfg/screens/settings/settings_screen.dart';
 
 import 'utils/index.dart';
 
+Future testSettingsScreen(WidgetTester tester) async {
+  final screen = Scaffold(body: SettingsScreen());
+  await tester.pumpWidget(buildTestableWidget(screen));
+  settingsMap.values.forEach((item) {
+    expect(find.text(item.text), findsOneWidget);
+  });
+}
+
+Future testCreditsScreen(WidgetTester tester) async {
+  final screen = Scaffold(body: CreditsScreen());
+  await tester.pumpWidget(buildTestableWidget(screen));
+  expect(find.text('Credits'), findsOneWidget);
+  expect(find.byType(Column), findsOneWidget);
+  expect(find.text('wwww.github.com/erikbg7/Flutter-Project'), findsOneWidget);
+}
+
+Future testPrivacyScreen(WidgetTester tester) async {
+  final screen = Scaffold(body: PrivacyScreen());
+  await tester.pumpWidget(buildTestableWidget(screen));
+  expect(find.text('Privacitat'), findsOneWidget);
+  expect(find.byType(Center), findsOneWidget);
+  expect(find.byType(Text), findsWidgets);
+}
+
 void main() {
-  testWidgets(
-    'Displays a page for Settings/Credits',
-    (WidgetTester tester) async {
-      await tester
-          .pumpWidget(buildTestableWidget(Scaffold(body: CreditsScreen())));
-      expect(find.text('Credits'), findsOneWidget);
-      expect(find.byType(Column), findsOneWidget);
-      expect(
-          find.text('wwww.github.com/erikbg7/Flutter-Project'), findsOneWidget);
-    },
-  );
-
-  testWidgets(
-    'Displays a page for Settings/Privacy',
-    (WidgetTester tester) async {
-      await tester
-          .pumpWidget(buildTestableWidget(Scaffold(body: PrivacyScreen())));
-      expect(find.text('Privacitat'), findsOneWidget);
-      expect(find.byType(Center), findsOneWidget);
-      expect(find.byType(Text), findsWidgets);
-    },
-  );
-
-  testWidgets(
-    'Displays a list of settings',
-    (WidgetTester tester) async {
-      await tester
-          .pumpWidget(buildTestableWidget(Scaffold(body: SettingsScreen())));
-
-      settingsMap.values.forEach((item) {
-        expect(find.text(item.text), findsOneWidget);
-      });
-    },
-  );
+  testWidgets('Displays a page for Settings/Credits', testCreditsScreen);
+  testWidgets('Displays a page for Settings/Privacy', testPrivacyScreen);
+  testWidgets('Displays a list of settings', testSettingsScreen);
 }
