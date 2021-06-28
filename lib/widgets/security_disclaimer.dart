3b641fc84a9e62ae3edfe2532b090024b8152985
ar-erik.blanca <erik.bg7@gmail.com>
2021-06-28 22:49:21 +0200

    Security disclaimer for dark theme

--- lib/widgets/security_disclaimer.dart
+++ lib/widgets/security_disclaimer.dart
@@ -5,15 +5,16 @@ class SecurityDisclaimer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(20),
+      margin: EdgeInsets.symmetric(vertical: 20, horizontal: 7),
       padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(color: Colors.blue, width: 2)),
+        color: Colors.grey.withOpacity(0.3),
+        borderRadius: BorderRadius.circular(10),
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Icon(Icons.warning_amber_rounded, size: 40),
+          Icon(Icons.warning_amber_rounded, size: 40, color: Colors.greenAccent,),
           SizedBox(width: 10),
           Flexible(
             child: Text(
