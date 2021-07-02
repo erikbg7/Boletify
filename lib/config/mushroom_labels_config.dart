2e54503c03f662f88a53738a20bb4ef7273898d8
ar-erik.blanca <erik.bg7@gmail.com>
2021-07-02 20:53:00 +0200

    New model property label

--- lib/config/mushroom_labels_config.dart
+++ lib/config/mushroom_labels_config.dart
@@ -5,36 +5,43 @@ Map<SearchLabels, MushroomLabel> mushroomLabels = {
   SearchLabels.spring: MushroomLabel(
     imageUrl: 'assets/season/spring.png',
     tooltip: 'Temporada de primavera',
+    label: 'Prmavera',
     colors: [Colors.pink, Colors.purple],
   ),
   SearchLabels.summer: MushroomLabel(
     imageUrl: 'assets/season/summer.png',
     tooltip: "Temporada d'estiu",
+    label: 'Estiu',
     colors: [Colors.yellow, Colors.orange],
   ),
   SearchLabels.autumn: MushroomLabel(
     imageUrl: 'assets/season/autum.png',
     tooltip: "Temporada de tardor",
+    label: 'Tardor',
     colors: [Colors.orange, Colors.red],
   ),
   SearchLabels.winter: MushroomLabel(
     imageUrl: 'assets/season/winter.png',
     tooltip: "Temporada d'hivern",
+    label: 'Hivern',
     colors: [Colors.blue, Colors.green],
   ),
   SearchLabels.edible: MushroomLabel(
     imageUrl: 'assets/edibility/edible.png',
     tooltip: 'Bon comestible',
+    label: 'Comestible',
     colors: [Colors.green, Colors.greenAccent],
   ),
   SearchLabels.toxic: MushroomLabel(
     imageUrl: 'assets/edibility/toxic.png',
     tooltip: 'Toxic, aluciogen o mortal',
+    label: 'Tóxic',
     colors: [Colors.yellow, Colors.grey],
   ),
   SearchLabels.unknown: MushroomLabel(
     imageUrl: 'assets/edibility/unknown.png',
     tooltip: 'Desconegut o sense informació',
+    label: 'Desconegut',
     colors: [Colors.grey, Colors.black],
   )
 };
--- lib/models/mushroom_label_model.dart
+++ lib/models/mushroom_label_model.dart
@@ -4,8 +4,9 @@ enum SearchLabels { spring, summer, autumn, winter, edible, toxic, unknown }
 
 class MushroomLabel {
   final String imageUrl;
+  final String label;
   final String tooltip;
   final List<Color> colors;
 
-  MushroomLabel({this.imageUrl, this.tooltip, this.colors});
+  MushroomLabel({this.imageUrl, this.label, this.tooltip, this.colors});
 }
