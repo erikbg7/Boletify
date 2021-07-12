f2250b09a75919e7017eb2c7c994c86bb4bfef30
ar-erik.blanca <erik.bg7@gmail.com>
2021-07-12 23:47:00 +0200

    Improve coverage :white_check_mark:

--- test/search_test.dart
+++ test/search_test.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:futter_project_tfg/models/mushroom_info_model.dart';
+import 'package:futter_project_tfg/models/mushroom_label_model.dart';
+import 'package:futter_project_tfg/screens/detail/components/detail_labels.dart';
+import 'package:futter_project_tfg/screens/search/components/filter_button.dart';
+import 'package:futter_project_tfg/screens/search/components/search_tile.dart';
 import 'package:futter_project_tfg/screens/search/search_screen.dart';
 
 import 'utils/index.dart';
@@ -9,6 +15,27 @@ Future testSearchScreen(WidgetTester tester) async {
   expect(find.text('No items'), findsOneWidget);
 }
 
+Future testFilterButton(WidgetTester tester) async {
+  final id = SearchLabels.winter;
+  final screen = Scaffold(
+    body: FilterButton(id: id, isActive: true, text: 'some text'),
+  );
+  await tester.pumpWidget(buildTestableWidget(screen));
+  expect(find.text('some text'), findsOneWidget);
+  expect(find.byType(FlatButton), findsOneWidget);
+}
+
+Future testSearchTile(WidgetTester tester) async {
+  final item = MushroomInfo('name', 'scientific', [SearchLabels.winter]);
+  final screen = Scaffold(body: SearchTile(item: item));
+  await tester.pumpWidget(buildTestableWidget(screen));
+  expect(find.text(item.name), findsOneWidget);
+  expect(find.text(item.nameScientific), findsOneWidget);
+  expect(find.byType(DetailLabels), findsOneWidget);
+}
+
 void main() {
   testWidgets('Displays Search screen without any items', testSearchScreen);
+  testWidgets('Displays a filter button', testFilterButton);
+  testWidgets('Displays a search tile', testSearchTile);
 }
