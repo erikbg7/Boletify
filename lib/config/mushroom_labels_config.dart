21953daf86a5c2c7afd0916787a4e3ff0fbc95c7
ar-erik.blanca <erik.bg7@gmail.com>
2021-07-24 11:41:26 +0200

    Null safety helper for new model :recycle:

--- lib/config/mushroom_labels_config.dart
+++ lib/config/mushroom_labels_config.dart
@@ -1,47 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:futter_project_tfg/models/mushroom_label_model.dart';
+import 'package:futter_project_tfg/models/tag_model.dart';
 
-Map<SearchLabels, MushroomLabel> mushroomLabels = {
-  SearchLabels.spring: MushroomLabel(
-    imageUrl: 'assets/season/spring.png',
-    tooltip: 'Temporada de primavera',
-    label: 'Primavera',
-    colors: [Colors.pink, Colors.purple],
-  ),
-  SearchLabels.summer: MushroomLabel(
-    imageUrl: 'assets/season/summer.png',
-    tooltip: "Temporada d'estiu",
-    label: 'Estiu',
-    colors: [Colors.yellow, Colors.orange],
-  ),
-  SearchLabels.autumn: MushroomLabel(
-    imageUrl: 'assets/season/autum.png',
-    tooltip: "Temporada de tardor",
-    label: 'Tardor',
-    colors: [Colors.orange, Colors.red],
-  ),
-  SearchLabels.winter: MushroomLabel(
-    imageUrl: 'assets/season/winter.png',
-    tooltip: "Temporada d'hivern",
-    label: 'Hivern',
-    colors: [Colors.blue, Colors.green],
-  ),
-  SearchLabels.edible: MushroomLabel(
-    imageUrl: 'assets/edibility/edible.png',
-    tooltip: 'Bon comestible',
-    label: 'Comestible',
-    colors: [Colors.green, Colors.greenAccent],
-  ),
-  SearchLabels.toxic: MushroomLabel(
-    imageUrl: 'assets/edibility/toxic.png',
-    tooltip: 'Toxic, aluciogen o mortal',
-    label: 'Tóxic',
-    colors: [Colors.yellow, Colors.grey],
-  ),
-  SearchLabels.unknown: MushroomLabel(
-    imageUrl: 'assets/edibility/unknown.png',
-    tooltip: 'Desconegut o sense informació',
-    label: 'Desconegut',
-    colors: [Colors.grey, Colors.black],
-  )
-};
+final TagInfo springMushroomInfo = TagInfo(
+  imageUrl: 'assets/season/spring.png',
+  tooltip: 'Temporada de primavera',
+  label: 'Primavera',
+  colors: [Colors.pink, Colors.purple],
+);
+
+final TagInfo summerMushroomInfo = TagInfo(
+  imageUrl: 'assets/season/summer.png',
+  tooltip: "Temporada d'estiu",
+  label: 'Estiu',
+  colors: [Colors.yellow, Colors.orange],
+);
+
+final TagInfo autumnMushroomInfo = TagInfo(
+  imageUrl: 'assets/season/autum.png',
+  tooltip: "Temporada de tardor",
+  label: 'Tardor',
+  colors: [Colors.orange, Colors.red],
+);
+
+final TagInfo winterMushroomInfo = TagInfo(
+  imageUrl: 'assets/season/winter.png',
+  tooltip: "Temporada d'hivern",
+  label: 'Hivern',
+  colors: [Colors.blue, Colors.green],
+);
+
+final TagInfo edibleMushroomInfo = TagInfo(
+  imageUrl: 'assets/edibility/edible.png',
+  tooltip: 'Bon comestible',
+  label: 'Comestible',
+  colors: [Colors.green, Colors.greenAccent],
+);
+
+final TagInfo toxicMushroomInfo = TagInfo(
+  imageUrl: 'assets/edibility/toxic.png',
+  tooltip: 'Toxic, aluciogen o mortal',
+  label: 'Tóxic',
+  colors: [Colors.yellow, Colors.grey],
+);
+
+final TagInfo unknownMushroomInfo = TagInfo(
+  imageUrl: 'assets/edibility/unknown.png',
+  tooltip: 'Desconegut o sense informació',
+  label: 'Desconegut',
+  colors: [Colors.grey, Colors.black],
+);
+
+TagInfo infoFromTag(Tag tag) {
+  switch (tag) {
+    case Tag.spring:
+      return springMushroomInfo;
+    case Tag.summer:
+      return summerMushroomInfo;
+    case Tag.autumn:
+      return autumnMushroomInfo;
+    case Tag.winter:
+      return winterMushroomInfo;
+    case Tag.edible:
+      return edibleMushroomInfo;
+    case Tag.toxic:
+      return toxicMushroomInfo;
+    default:
+      return unknownMushroomInfo;
+  }
+}
