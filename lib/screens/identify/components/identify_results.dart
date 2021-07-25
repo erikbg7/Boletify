2f07aaa4863f530abe21794b832a927b14cd1534
ar-erik.blanca <erik.bg7@gmail.com>
2021-07-25 20:11:19 +0200

    Show different result views based on confidence level :sparkles:

--- lib/screens/identify/components/identify_results.dart
+++ lib/screens/identify/components/identify_results.dart
@@ -1,34 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:futter_project_tfg/models/classifier_output_model.dart';
-import 'package:futter_project_tfg/models/tag_model.dart';
-import 'package:futter_project_tfg/screens/detail/components/detail_labels.dart';
-import 'package:futter_project_tfg/screens/identify/components/identify_title.dart';
-import 'package:futter_project_tfg/screens/identify/components/identify_image.dart';
+import 'package:futter_project_tfg/screens/identify/results/confidence.dart';
+
+isHighConfidence(ClassifierOutput result) => result.confidence > 0.95;
+
+isMediumConfidence(ClassifierOutput result) => result.confidence > 0.50;
 
 class IdentifyResults extends StatelessWidget {
   final ClassifierOutput result;
-
   const IdentifyResults({Key? key, required this.result}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        IdentifyTitle(text: 'RESULTATS'),
-        IdentifyImage(image: result.image),
-        SizedBox(height: 20),
-        DetailLabels(tags: [Tag.toxic, Tag.autumn]),
-        SizedBox(width: double.infinity, height: 15),
-        Text('Amanita'.toUpperCase(),
-            style: TextStyle(fontFamily: 'Milliard', fontSize: 20)),
-        Text(
-          'Amanita Muscaria',
-          style: TextStyle(
-              fontFamily: 'Milliard', fontSize: 20, color: Colors.white70),
-        ),
-        Text("N'estic ${(result.confidence*100).toStringAsFixed(2)}% segur!")
-      ],
-    );
+    if (isHighConfidence(result)) return HighConfidence(result: result);
+    if(isMediumConfidence(result)) return MediumConfidence(result: result);
+    return LowConfidence(result: result);
   }
 }
