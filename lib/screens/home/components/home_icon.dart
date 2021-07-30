01009a800972def12820f411960787ea76c4db06
ar-erik.blanca <erik.bg7@gmail.com>
2021-07-31 00:04:02 +0200

    Dummy filter action in search appbar :lipstick:

--- lib/screens/home/components/home_icon.dart
+++ lib/screens/home/components/home_icon.dart
@@ -25,7 +25,20 @@ class HomeIcon extends StatelessWidget {
           context,
           MaterialPageRoute<void>(builder: (BuildContext context) {
             return Scaffold(
-              appBar: AppBar(title: Text('Búsqueda')),
+              appBar: AppBar(
+                title: Text('Búsqueda'),
+//                actions: [
+//                  Padding(
+//                      padding: EdgeInsets.only(right: 20.0),
+//                      child: GestureDetector(
+//                        onTap: () {},
+//                        child: Icon(
+//                          Icons.filter_list,
+//                          size: 26.0,
+//                        ),
+//                      )),
+//                ],
+              ),
               body: SearchScreen(filter: [tag], mushroomsList: mushroomList),
             );
           }),
