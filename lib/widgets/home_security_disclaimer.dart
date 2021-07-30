46d1ece40489ff5e1359c5f9197827f1f8f98a11
ar-erik.blanca <erik.bg7@gmail.com>
2021-07-30 23:35:23 +0200

    Fix security disclaimer spacing :recycle:

--- lib/widgets/home_security_disclaimer.dart
+++ lib/widgets/home_security_disclaimer.dart
@@ -7,33 +7,36 @@ class HomeSecurityDisclaimer extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+        child: Stack(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Center(
+              child: Icon(Icons.warning_rounded,
+                  size: 175, color: Colors.redAccent.withOpacity(0.5)),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                SizedBox(height: 15),
                 Text(
                   'Atenció',
                   style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 25,
-                      fontWeight: FontWeight.w500),
-                ),
-                Icon(Icons.warning_rounded,
-                    size: 50, color: Colors.greenAccent),
-              ],
-            ),
-            SizedBox(width: 10),
-            Center(
-              child: Text(
-                "Boletify es una aplicació experimental. Utilitza les teves fonts d'informació habituals per saber si el bolet es comestible o tòxic,  recorda que alguns bolets poden ser mortals i és facil confondre'ls.",
-                softWrap: true,
-                style: TextStyle(
-                    height: 1.5,
                     color: Colors.black,
-                    fontWeight: FontWeight.w500),
-              ),
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  "Boletify es una aplicació experimental. Utilitza les teves fonts d'informació habituals per saber si el bolet es comestible o tòxic,  recorda que alguns bolets poden ser mortals i és facil confondre'ls.",
+                  softWrap: true,
+                  style: TextStyle(
+                      height: 1.5,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16),
+                ),
+              ],
             ),
           ],
         ),
@@ -41,3 +44,45 @@ class HomeSecurityDisclaimer extends StatelessWidget {
     );
   }
 }
+
+//class HomeSecurityDisclaimer extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Expanded(
+//      child: Container(
+//        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+//        color: Colors.white,
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.start,
+//          children: [
+//            Row(
+//              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//              children: [
+//                Text(
+//                  'Atenció',
+//                  style: TextStyle(
+//                      color: Colors.black,
+//                      fontSize: 25,
+//                      fontWeight: FontWeight.w500),
+//                ),
+//                Icon(Icons.warning_rounded,
+//                    size: 50, color: Colors.greenAccent),
+//              ],
+//            ),
+//            SizedBox(width: 10),
+//            Center(
+//              child: Text(
+//                "Boletify es una aplicació experimental. Utilitza les teves fonts d'informació habituals per saber si el bolet es comestible o tòxic,  recorda que alguns bolets poden ser mortals i és facil confondre'ls.",
+//                softWrap: true,
+//                style: TextStyle(
+//                    height: 1.5,
+//                    color: Colors.black,
+//                    fontWeight: FontWeight.w500),
+//              ),
+//            ),
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
