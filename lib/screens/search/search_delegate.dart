b0e3da7d36c093828e3d461663dc2a77b43b6bd1
ar-erik.blanca <erik.bg7@gmail.com>
2021-08-01 16:48:58 +0200

    Delegate actions :sparkles:

--- lib/screens/search/search_delegate.dart
+++ lib/screens/search/search_delegate.dart
@@ -9,30 +9,27 @@ class NameSearch extends SearchDelegate<MushroomInfo> {
 
   NameSearch({required this.mushrooms});
 
-
   @override
   ThemeData appBarTheme(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     return theme;
   }
-//
-//  @override
-//  List<Widget> buildActions(BuildContext context) {
-//    return [
-//      IconButton(
-//        icon: Icon(Icons.clear),
-//        onPressed: () {
-//          query = '';
-//        },
-//      )
-//    ];
-//  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () => query = '',
+      )
+    ];
+  }
 
   @override
   Widget buildLeading(BuildContext context) {
     return IconButton(
       icon: Icon(Icons.arrow_back),
-      onPressed: () => close(context, result)
+      onPressed: () => close(context, result),
     );
   }
 //
