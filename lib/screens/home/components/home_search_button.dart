aec6bb57fd7fcb8040b93601d830e37ba635ab59
ar-erik.blanca <erik.bg7@gmail.com>
2021-08-02 19:27:57 +0200

    Use delegate in search home widget :recycle:

--- lib/screens/home/components/home_search_button.dart
+++ lib/screens/home/components/home_search_button.dart
@@ -14,7 +14,7 @@ class HomeSearchButton extends StatelessWidget {
         onPressed: () async {
           await showSearch<MushroomInfo>(
             context: context,
-            delegate: NameSearch(mushrooms: getMushroomsListMock()),
+            delegate: MushroomSearchDelegate(mushrooms: getMushroomsListMock()),
           );
         },
         style: ButtonStyle(
