c90279088a78c6a1d02d060237ee95a549539abf
ar-erik.blanca <erik.bg7@gmail.com>
2021-08-08 20:01:38 +0200

    Wrap inkwell with material :sparkles:

--- lib/screens/search/components/search_tile.dart
+++ lib/screens/search/components/search_tile.dart
@@ -12,41 +12,43 @@ class SearchTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => redirectTo(
-          context: context,
-          screen: DetailScreen(
-              image: File('assets/search_background/0.jpg'), mushroom: item)),
-      child: Container(
-        margin: EdgeInsets.only(bottom: 5),
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        width: double.infinity,
-        color: Colors.black26,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              width: 50,
-              height: 50,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/search_background/2.jpg'),
-                    fit: BoxFit.fill),
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(100),
+    return Material(
+      child: InkWell(
+        onTap: () => redirectTo(
+            context: context,
+            screen: DetailScreen(
+                image: File('assets/search_background/0.jpg'), mushroom: item)),
+        child: Container(
+          margin: EdgeInsets.only(bottom: 5),
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          width: double.infinity,
+          color: Colors.black26,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/search_background/2.jpg'),
+                      fit: BoxFit.fill),
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(100),
+                ),
               ),
-            ),
-            SizedBox(width: 10),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(item.name, style: TextStyle(fontSize: 20)),
-                Text(item.scientificName,
-                    style: TextStyle(fontSize: 15, color: Colors.white70)),
-              ],
-            ),
-          ],
+              SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(item.name, style: TextStyle(fontSize: 20)),
+                  Text(item.scientificName,
+                      style: TextStyle(fontSize: 15, color: Colors.white70)),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
