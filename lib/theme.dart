bb1c596eb0a41c555d02b32d342a9059359083c1
ar-erik.blanca <erik.bg7@gmail.com>
2021-08-15 20:29:39 +0200

    Add styles theme and test utils :art:

---
+++ lib/theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  TextStyles();
+
+  static const TextStyle detailCardTitle = const TextStyle(
+    color: Color(0xFFFFFFFF),
+    fontSize: 23,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle detailCardSubtitle = const TextStyle(
+    color: Color(0x9FFFFFFF),
+  );
+
+  static const TextStyle detailSectionTitle = const TextStyle(
+    color: Color(0xFFFFFFFF),
+    fontSize: 16,
+  );
+
+  static const TextStyle detailSectionText = const TextStyle(
+    color: Color(0x9FFFFFFF),
+    fontSize: 14,
+  );
+}
