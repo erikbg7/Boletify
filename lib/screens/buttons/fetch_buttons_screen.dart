250454f12c4b8ff23228f08ee49c8580aab464f6
ar-erik.blanca <erik.bg7@gmail.com>
2021-09-05 14:14:12 +0200

    Improve fetch buttons test coverage :white_check_mark:

--- lib/screens/buttons/fetch_buttons_screen.dart
+++ lib/screens/buttons/fetch_buttons_screen.dart
@@ -6,20 +6,10 @@ class FetchButtonsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Icons"),
-      ),
+      appBar: AppBar(title: Text("Icons")),
       body: BlocProvider(
         create: (BuildContext context) => MushroomsBloc(),
-        child: Container(
-          color: Colors.black38,
-          height: double.infinity,
-          width: double.infinity,
-          child: Center(
-            child: BlocBuilder<MushroomsBloc, MushroomsState>(
-                builder: (context, state) => FetchButtons()),
-          ),
-        ),
+        child: FetchButtons(),
       ),
     );
   }
@@ -28,24 +18,28 @@ class FetchButtonsScreen extends StatelessWidget {
 class FetchButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextButton(
-          style: TextButton.styleFrom(primary: Colors.blue),
-          onPressed: () {
-            BlocProvider.of<MushroomsBloc>(context)
-                .add(FindMushroomById('Amanita'));
-          },
-          child: Text('FindByName'),
+    return BlocBuilder<MushroomsBloc, MushroomsState>(
+      builder: (context, state) => Center(
+        child: Column(
+          children: [
+            TextButton(
+              style: TextButton.styleFrom(primary: Colors.blue),
+              onPressed: () {
+                BlocProvider.of<MushroomsBloc>(context)
+                    .add(FindMushroomById('Amanita'));
+              },
+              child: Text('FindByName'),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(primary: Colors.blue),
+              onPressed: () {
+                BlocProvider.of<MushroomsBloc>(context).add(GetAllMushrooms());
+              },
+              child: Text('GetAllMuhsrooms'),
+            ),
+          ],
         ),
-        TextButton(
-          style: TextButton.styleFrom(primary: Colors.blue),
-          onPressed: () {
-            BlocProvider.of<MushroomsBloc>(context).add(GetAllMushrooms());
-          },
-          child: Text('GetAllMuhsrooms'),
-        ),
-      ],
+      ),
     );
   }
 }
--- test/fetch_buttons_test.dart
+++ test/fetch_buttons_test.dart
@@ -1,18 +1,71 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:futter_project_tfg/bloc/mushrooms/mushrooms_bloc.dart';
 import 'package:futter_project_tfg/screens/buttons/fetch_buttons_screen.dart';
 
 import 'utils/index.dart';
 
-Future testFetchButtonsScreen(WidgetTester tester) async {
-  final screen = Scaffold(body: FetchButtons());
-  await tester.pumpWidget(buildTestableWidget(screen));
+class MockMushroomsBloc extends MockBloc<MushroomsEvent, MushroomsState>
+    implements MushroomsBloc {}
 
-  expect(find.byType(TextButton), findsNWidgets(2));
-  expect(find.text('FindByName'), findsOneWidget);
-  expect(find.text('GetAllMuhsrooms'), findsOneWidget);
-}
+class FallbackState extends Fake implements MushroomsState {}
+
+class FallbackEvent extends Fake implements MushroomsEvent {}
+
 
 void main() {
-  testWidgets('HomeIcon widget', testFetchButtonsScreen);
+  late MushroomsBloc mockMushroomsBloc;
+
+  setUpAll(() {
+    registerFallbackValue<MushroomsState>(FallbackState());
+    registerFallbackValue<MushroomsEvent>(FallbackEvent());
+  });
+
+  setUp(() {
+    mockMushroomsBloc = MockMushroomsBloc();
+  });
+
+  tearDown(() {
+    mockMushroomsBloc.close();
+  });
+
+  testWidgets(
+    'Displays Search screen without any items',
+    (WidgetTester tester) async {
+      final testWidget = BlocProvider.value(
+        value: mockMushroomsBloc,
+        child: FetchButtons(),
+      );
+
+      when(() => mockMushroomsBloc.state).thenReturn(MushroomsInitial());
+
+      await tester.pumpWidget(buildTestableWidget(testWidget));
+      expect(find.byType(Center), findsOneWidget);
+      expect(find.byType(Column), findsOneWidget);
+      expect(find.byType(TextButton), findsNWidgets(2));
+      expect(find.text('FindByName'), findsOneWidget);
+      expect(find.text('GetAllMuhsrooms'), findsOneWidget);
+
+      await tester.press(find.text('FindByName'));
+      await tester.pump(Duration(seconds: 1));
+
+      expect(find.byType(Center), findsOneWidget);
+      expect(find.byType(Column), findsOneWidget);
+      expect(find.byType(TextButton), findsNWidgets(2));
+      expect(find.text('FindByName'), findsOneWidget);
+      expect(find.text('GetAllMuhsrooms'), findsOneWidget);
+
+      await tester.press(find.text('GetAllMuhsrooms'));
+      await tester.pump(Duration(seconds: 1));
+
+      expect(find.byType(Center), findsOneWidget);
+      expect(find.byType(Column), findsOneWidget);
+      expect(find.byType(TextButton), findsNWidgets(2));
+      expect(find.text('FindByName'), findsOneWidget);
+      expect(find.text('GetAllMuhsrooms'), findsOneWidget);
+    },
+  );
 }
