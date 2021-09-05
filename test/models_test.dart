6b7499b149822365b8404aa0eac3b304a96cfef2
ar-erik.blanca <erik.bg7@gmail.com>
2021-09-05 21:55:49 +0200

    Test ClassifierOutput.fromTFLite :white_check_mark:

--- test/models_test.dart
+++ test/models_test.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:futter_project_tfg/models/classifier_output_model.dart';
 import 'package:futter_project_tfg/models/mushroom_info_model.dart';
 import 'package:futter_project_tfg/models/tag_model.dart';
 
@@ -6,6 +9,21 @@ testTagsFromAndToJSON() {
   final List<Tag> tags = [Tag.winter, Tag.edible];
   final List<Tag> deserializedTags = tagsFromJson(tagsToJson(tags));
   tags.forEach((tag) => expect(deserializedTags.contains(tag), true));
+}
+
+testOutputFromTFLite() {
+  final double confidence = 0.9;
+  final String label = labelsMap.keys.first;
+  final File image = new File('imageurl');
+
+  Map<String, dynamic> tfresult = {'confidence': confidence, 'label': label};
+  List<dynamic> data = [tfresult].toList();
+
+  ClassifierOutput output = ClassifierOutput.fromTFLite(data, image);
+
+  expect(output.confidence == confidence, true);
+  expect(output.label == labelsMap[label], true);
+  expect(output.image == image, true);
 }
 
 testMockedMushroomList() {
@@ -45,6 +63,7 @@ testMushroomDescriptionFromToJSON() {
 void main() {
   test('MushroomInfo model has mocked list', testMockedMushroomList);
   test('Tags to JSON and from JSON', testTagsFromAndToJSON);
+  test('ClassifierOutput from TFLite', testOutputFromTFLite);
   test('MushroomDescription from/to JSON', testMushroomDescriptionFromToJSON);
   test('Mushroom info from Firestore', testMushroomsFromFirestore);
 }
