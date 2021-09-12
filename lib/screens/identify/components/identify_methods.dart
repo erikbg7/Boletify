aad88f8488d238440a7bcab575da4bfa3df3b2ab
ar-erik.blanca <erik.bg7@gmail.com>
2021-09-12 16:55:33 +0200

    Encapsulate classifier logic in classifier BLoC :recycle:

--- lib/screens/identify/components/identify_methods.dart
+++ lib/screens/identify/components/identify_methods.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:futter_project_tfg/bloc/mushrooms/mushrooms_bloc.dart';
+import 'package:futter_project_tfg/bloc/classifier/classifier_bloc.dart';
 import 'package:futter_project_tfg/screens/identify/components/identify_button.dart';
 import 'package:futter_project_tfg/screens/identify/components/identify_title.dart';
 import 'package:image_picker/image_picker.dart';
@@ -24,16 +24,16 @@ class IdentifyMethods extends StatelessWidget {
                   text: 'Camara',
                   icon: Icons.photo_camera_outlined,
                   onPressed: () {
-                    BlocProvider.of<MushroomsBloc>(context)
-                        .add(ClassifyMushroom(ImageSource.camera));
+                    BlocProvider.of<ClassifierBloc>(context)
+                        .add(ClassifyEvent(ImageSource.camera));
                   },
                 ),
                 IdentifyButton(
                   text: 'Galeria',
                   icon: Icons.broken_image_outlined,
                   onPressed: () {
-                    BlocProvider.of<MushroomsBloc>(context)
-                        .add(ClassifyMushroom(ImageSource.gallery));
+                    BlocProvider.of<ClassifierBloc>(context)
+                        .add(ClassifyEvent(ImageSource.gallery));
                   },
                 ),
               ],
